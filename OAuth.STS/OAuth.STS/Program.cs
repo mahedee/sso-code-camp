32199e37e62cfe1383e0b6be6cafe8de69e6a61b
mahedee <mahedee.hasan@gmail.com>
2022-04-18 10:40:52 +0600

    ...

---
+++ OAuth.STS/OAuth.STS/Program.cs
@@ -0,0 +1,39 @@
+using OAuth.STS.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add identity server to the application
+builder.Services.AddIdentityServer()
+    .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+    .AddTestUsers(InMemoryConfig.GetUsers())
+    .AddInMemoryClients(InMemoryConfig.GetClients())
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Add identity server to request pipeline
+app.UseIdentityServer();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
